3773ad6149cddb581840d4dd8a2f9e734da1ee74
kingsantha <kingsantha16af@yahoo.com>
2017-05-04 16:40:07 +0700

    Update pagenotfound.php

--- pagenotfound.php
+++ pagenotfound.php
@@ -32,7 +32,7 @@
 			<div class="error-page">
             <h2 class="headline text-yellow"> 404</h2>
             <div class="error-content">
-              <h3><i class="fa fa-warning text-yellow"></i> Oops! Page not found.</h3>
+              <h3><i class="fa fa-warning text-yellow"></i> Oops sorry! Page not found.</h3>
               <p>
                 We could not find the page you were looking for.
                 Meanwhile, you may <a href="index.php">return to Login !</a>
